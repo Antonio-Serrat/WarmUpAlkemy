456f5253315d988bf695f731087ff4326c9e0371
Tonisrt <4antoniogabriel20@gmail.com>
2021-06-15 10:28:14 -0300

    Initialized project

---
+++ src/main/java/WUpAlkemy/WarmUpAlkemyApplication.java
@@ -0,0 +1,13 @@
+package WUpAlkemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmUpAlkemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmUpAlkemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WUpAlkemy/WarmUpAlkemyApplicationTests.java
@@ -0,0 +1,13 @@
+package WUpAlkemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmUpAlkemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
