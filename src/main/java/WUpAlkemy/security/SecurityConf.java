b289a31c3040b9c13c3da685b51b5f851ae1a0e5
Tonisrt <4antoniogabriel20@gmail.com>
2021-06-16 11:07:59 -0300

    Add JWT to pom

--- src/main/java/WUpAlkemy/security/SecurityConf.java
+++ src/main/java/WUpAlkemy/security/SecurityConf.java
@@ -3,11 +3,13 @@ package WUpAlkemy.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import WUpAlkemy.service.UserDetailsIm;
 
@@ -26,8 +28,9 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/GET/**", "/POST/auth/**").permitAll()
-				.antMatchers("/POST/posts", "/PATCH/**").hasRole("USER").antMatchers("/DELETE/**").hasAnyRole("ADMIN")
+		http.csrf().disable().addFilterAfter(new JwtAuth(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests().antMatchers(HttpMethod.POST, "/", "/GET/**", "/POST/auth/**").permitAll()
+//				.antMatchers("/POST/posts", "/PATCH/**").hasRole("USER").antMatchers("/DELETE/**").hasAnyRole("ADMIN")
 				.anyRequest().authenticated().and().formLogin().permitAll().and().logout().logoutSuccessUrl("/");
 
 	}
