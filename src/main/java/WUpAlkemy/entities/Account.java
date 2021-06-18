334e4c6beacda65bf81d39a3b865a7d698ce6f19
Tonisrt <4antoniogabriel20@gmail.com>
2021-06-18 18:26:56 -0300

    delete one atribute

--- src/main/java/WUpAlkemy/entities/Account.java
+++ src/main/java/WUpAlkemy/entities/Account.java
@@ -37,8 +37,6 @@ public class Account implements Serializable {
 	private Blogger blogger;
 
 	private String username;
-
-	private String email = username;
 	private String password;
 
 	public void setOwner(Blogger blogger) {
