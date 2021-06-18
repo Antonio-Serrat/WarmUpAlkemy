41a1f87819078863f695d9e656fda10bf572bf7a
Tonisrt <4antoniogabriel20@gmail.com>
2021-06-18 18:26:19 -0300

    add account dto

---
+++ src/main/java/WUpAlkemy/DTOs/AccountDTO.java
@@ -0,0 +1,14 @@
+package WUpAlkemy.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDTO {
+
+	private String email;
+	private String pass;
+}
